c966ce257f218b2e151f56e7876f2cdecd760b18
muchu999 <christi999@hotmail.com>
2026-06-22 17:58:26 -0400

    Going back to waitable object again now that other things have changed

--- xbmc/rendering/dx/DeviceResources.cpp
+++ xbmc/rendering/dx/DeviceResources.cpp
@@ -797,8 +797,8 @@ void DX::DeviceResources::ResizeBuffers()
     m_swapChain->GetDesc1(&scDesc);
     hr = m_swapChain->ResizeBuffers(scDesc.BufferCount, lround(m_outputSize.Width),
                                     lround(m_outputSize.Height), scDesc.Format,
-	                                //DXGI_SWAP_CHAIN_FLAG_FRAME_LATENCY_WAITABLE_OBJECT | (windowed ? 0 : DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH));
-	                                (windowed ? 0 : DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH));
+	                                DXGI_SWAP_CHAIN_FLAG_FRAME_LATENCY_WAITABLE_OBJECT | (windowed ? 0 : DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH));
+	                                //(windowed ? 0 : DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH));
 	NotifySwapchainListeners("CreateSwapChain");
 
     if (hr == DXGI_ERROR_DEVICE_REMOVED || hr == DXGI_ERROR_DEVICE_RESET)
@@ -857,8 +857,8 @@ void DX::DeviceResources::ResizeBuffers()
     swapChainDesc.SwapEffect = CSysInfo::IsWindowsVersionAtLeast(CSysInfo::WindowsVersionWin10)
                                    ? DXGI_SWAP_EFFECT_FLIP_DISCARD
                                    : DXGI_SWAP_EFFECT_FLIP_SEQUENTIAL;
-    //swapChainDesc.Flags = DXGI_SWAP_CHAIN_FLAG_FRAME_LATENCY_WAITABLE_OBJECT | (windowed ? 0 : DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH);
-	swapChainDesc.Flags = (windowed ? 0 : DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH);
+    swapChainDesc.Flags = DXGI_SWAP_CHAIN_FLAG_FRAME_LATENCY_WAITABLE_OBJECT | (windowed ? 0 : DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH);
+	//swapChainDesc.Flags = (windowed ? 0 : DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH);
 	swapChainDesc.AlphaMode = DXGI_ALPHA_MODE_IGNORE;
     swapChainDesc.SampleDesc.Count = 1;
     swapChainDesc.SampleDesc.Quality = 0;
@@ -910,10 +910,10 @@ void DX::DeviceResources::ResizeBuffers()
 	HRESULT hr = swapChain->QueryInterface(__uuidof(IDXGISwapChain2), (void**) &swapChain2);
 	if(SUCCEEDED(hr) && swapChain2)
 	{
-	  //ComPtr<IDXGIDevice1> dxgiDevice;
-	  //hr = m_d3dDevice.As(&dxgiDevice); CHECK_ERR();
+	  ComPtr<IDXGIDevice1> dxgiDevice;
+	  hr = m_d3dDevice.As(&dxgiDevice); CHECK_ERR();
 	  HRESULT hr = swapChain2->SetMaximumFrameLatency(1);
-	  //dxgiWaitHandle = swapChain2->GetFrameLatencyWaitableObject();
+	  dxgiWaitHandle = swapChain2->GetFrameLatencyWaitableObject();
 	  swapChain2->Release();
 	}
 #endif
@@ -1352,7 +1352,7 @@ void DX::DeviceResources::Present()
   DXGI_PRESENT_PARAMETERS parameters = {};
   UINT64 start = CurrentHostCounter();
   //DX::DeviceResources::Get()->GetImmediateContext()->Flush();
-  HRESULT hr = m_swapChain->Present1(0, 0, &parameters);
+  HRESULT hr = m_swapChain->Present1(1, 0, &parameters);
   //DwmFlush();
   UINT64 end = CurrentHostCounter();
   UINT64 presentDuration = end - start;
@@ -1372,8 +1372,15 @@ void DX::DeviceResources::Present()
   // Log
   CLog::LogFC(LOGDEBUG, LOGAVTIMING, "Present duration: {:.3f} ms, Present period: {:.3f} ms, PresentCount = {}, PresentRefreshCount = {}", (double) presentDuration / freq*1000.0, (double) period / freq*1000.0, PresentCount, PresentRefreshCount);
 
-  // Pacer
-  Pacer.Update(m_swapChain);
+  //cl int64_t start2 = CurrentHostCounter();
+DWORD waitResult = WaitForSingleObjectEx(DX::DeviceResources::Get()->dxgiWaitHandle, 1000, TRUE); // Block until the DXGI hardware queue is ready to accept a frame
+//int64_t end2 = CurrentHostCounter();
+//static INT64 lastEnd2 = 0;
+//CLog::LogF(LOGDEBUG, "Wait duration: {:f} ms, inter post wait: {:f} ms", (end2 - start2) / (float) freq * 1000, (end2 - lastEnd2) / (float) freq * 1000);
+//lastEnd2 = end2;
+
+// Pacer
+//  Pacer.Update(m_swapChain);
 
   // If the device was removed either by a disconnection or a driver upgrade, we must recreate all device resources.
   if (hr == DXGI_ERROR_DEVICE_REMOVED || hr == DXGI_ERROR_DEVICE_RESET)
